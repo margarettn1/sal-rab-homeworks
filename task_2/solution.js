7e9076b5bc059ac25905adac8cf45a0eec525d20
Margarita_Nazarenko <margarettn@yandex.ru>
2022-11-09 00:52:25 +0300

    Задание 2.3 в6

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,7 +6,7 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
-    let shippingSum = 200;
+    let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
     If (productsSum = 0); {
