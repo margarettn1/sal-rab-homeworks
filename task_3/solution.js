35cf6318d0d0695eccede951dbdd148f3da92a9a
Margarita_Nazarenko <margarettn@yandex.ru>
2022-11-14 21:53:27 +0300

    Задание 3.1 в7

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,14 +4,10 @@ function renderCartItem(item) {
     // Задание №3.1. Формирование строки корзины
 
     product = {id: 1, title: 'Пицца', count: 5, price: '500.00'}; // product - объект вида {id: id, title: title, count: count, price: price}
-    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
-    // id - идентификатор
-    // title - наименование
-    // price - цена
-    // count - количество
+    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где id - идентификатор, title - наименование, price - цена, count - количество
 
     // сформируйте строку itemCountText вида: количество × цена ₽ = стоимость ₽,  например, 5 × 500.00 ₽ = 2500 ₽
-    let itemCountText = ` ` // 1. Создайте переменную itemCountText, равную пустой строке
+    let itemCountText = ` `; // 1. Создайте переменную itemCountText, равную пустой строке
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
